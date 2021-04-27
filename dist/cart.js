78d3c72e6d4e6bc899ecc4222919b882678f2162
imaqtbike <nguyen.cuonguuu@gmail.com>
2021-04-27 22:23:25 +0700

    finish add to cart function

--- dist/cart.js
+++ dist/cart.js
@@ -1,1 +1,77 @@
+'use strict';
+const deleteButton = document.getElementsByClassName('delete--item');
+const cartValue = document.querySelector('.cart__value');
+const tableBody = document.querySelector('.table__body');
+const totalValue = document.querySelector('.total__value');
 
+
+function loadCart() {
+    let products = JSON.parse(localStorage.getItem('products'));
+    let cartTotalItem = document.querySelector('.cart__value');
+    cartTotalItem.textContent = products.length;
+    products.forEach((product, index) => {
+        let cartItem = document.createElement('tr');
+        cartItem.className = 'cart-item';
+        cartItem.innerHTML = `<td><img src="${product.imgSrc}" alt='product image'></td>
+                  <td>${product.name}</td>
+                  <td><span class='cart-price'>${product.price}.000 vnd</span></td>
+                  <td>
+                    <button data-action="remove" id="btn-remove">-</button>
+                    <input name='product' class="product__quantity" id='product__quantity-${index}' type="number" min='1' max='100' value='${product.count}'>
+                    <button data-action="add" id='btn-add'>+</button>
+                  </td>
+                  <td><span class='cart-total-value'>${(parseInt(product.price) * product.count)
+            .toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ".")}.000 vnd
+                  </span></td>
+                  <td>
+                    <button class="delete--item"><i class="far fa-trash-alt"></i></button>
+                  </td>`;
+        tableBody.appendChild(cartItem);
+    });
+    listenersForButton();
+    updateCartTotal();
+}
+
+const listenersForButton = () => {
+    const increaseButton = document.querySelectorAll('#btn-add');
+    const decreaseButton = document.querySelectorAll('#btn-remove');
+    for (let i = 0; i < increaseButton.length; i++) {
+        increaseButton[i].addEventListener('click', (e) => {
+            updateQuantity(e, i);
+        });
+    }
+    ;
+    for (let i = 0; i < decreaseButton.length; i++) {
+        decreaseButton[i].addEventListener('click', (e) => {
+            updateQuantity(e, i);
+        });
+    }
+};
+
+const updateQuantity = (e, index) => {
+    const handleButtons = e.target.dataset.action;
+    let products = JSON.parse(localStorage.getItem('products'));
+    if (handleButtons === 'add') {
+        let quantity = document.getElementById(`product__quantity-${index}`);
+        quantity = products[index].count++;
+    }
+    if (handleButtons === 'remove') {
+        let quantity = document.getElementById(`product__quantity-${index}`);
+        quantity = products[index].count--;
+        if (products[index].count <= 0) {
+            products.splice(products.indexOf(products[index]), 1);
+        }
+    }
+    localStorage.setItem('products', JSON.stringify(products));
+    tableBody.innerHTML = '';
+    loadCart();
+};
+
+function updateCartTotal() {
+    let total = 0;
+    let products = JSON.parse(localStorage.getItem('products'));
+    total = products.reduce((acc, cur) => acc + cur.price * cur.count, 0);
+    let totalValue = document.querySelector('.total__value');
+    totalValue.textContent = `${total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}.000 vnd`
+}
--- js/grid.js
+++ js/grid.js
@@ -1,13 +1,27 @@
 "use strict";
+const productGrid = document.querySelector('.grid__product');
+let products = [];
+eventListeners();
+
+//all event listeners
+function eventListeners() {
+    window.addEventListener('DOMContentLoaded', () => {
+        loadJSON();
+        loadCart();
+    });
+    productGrid.addEventListener('click', purchaseProduct);
+}
+
+//load json file into grid display
 function loadJSON() {
     fetch('http://localhost:3000/products').then(response => {
         response.json().then(data => {
-            let html = '';
+            let html = '';                  
             data.forEach(product => {
-                html += `<div class="legacy__items__detail legacy-detail5"><img src="${product.img}" alt="OHUI">
+                html += `<div class="legacy__items__detail" '><img class='product__img' src="${product.img}" alt="OHUI">
                     <div class="legacy__items__detail__content legacy-content">
-                    <h4>${product.productName}</h4><a href="">
-                    <p>${product.name}</p></a><span class="price">${product.price}<small>vnd</small></span>
+                    <h4 class='product__name'>${product.productName}</h4><a href="">
+                    <p class='product__category'>${product.name}</p></a><span class="price">${product.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}<small>vnd</small></span>
                     </div>
                     <div class="legacy__items__detail__icon">
                     <div class="legacy-cta">
@@ -16,10 +30,51 @@ function loadJSON() {
                     </div>
                     </div>
                     </div>`;
-        })
-            const productGrid = document.querySelector('.grid__product');
-            productGrid.innerHTML = html;
-    })
-})}
-    
-loadJSON();
+            });
+             productGrid.innerHTML = html;
+        });
+    });
+}
+
+
+function purchaseProduct(e) {
+    if (e.target.classList.contains('add-to-cart-btn')) {
+        let product = e.target.parentElement.parentElement.parentElement;
+        getProductInfo(product);
+    }
+}
+
+
+//get product info after add-cart btn is clicked
+function getProductInfo(product) {
+    let productInfo = {
+        name: product.querySelector('.product__name').textContent,
+        imgSrc: product.querySelector('.product__img').src,
+        category: product.querySelector('.product__category').textContent,
+        price: parseInt(product.querySelector('.price').textContent),
+        count: 1,
+    };
+    pushItemInArray(productInfo);
+}
+
+function pushItemInArray(item) {
+    if (products.length === 0) {
+        products.push(item);
+        alert(`Đã thêm thành công sản phẩm vào giỏ hàng!`)
+        localStorage.setItem('products', JSON.stringify(products));
+        return;
+    }
+    for (let i = 0; i <= products.length; i++) {
+        if (products[i].name === item.name) {
+            products[i].count++;
+            localStorage.setItem('products', JSON.stringify(products));
+            return;
+        } else if (i === products.length - 1) {
+            products.push(item);
+            alert(`Đã thêm thành công sản phẩm vào giỏ hàng!`)
+            localStorage.setItem('products', JSON.stringify(products));
+            return;
+        }
+    }
+    products.push(item);
+}
